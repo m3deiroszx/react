903eb5023a7ba1bd22a336ce0412c80c0000bf21
GIOVANNA MEDEIROS DA SILVA <25162400@edu119.local>
2026-02-11 16:12:57 -0300

    tela de login em react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <p>Entre com suas credenciais para acessar o sistema</p>
+
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Senha" />
+
+      <button>Entrar</button>
+    </main>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+import "./styles/theme.css";
+import "./styles/global.css";
+
+createRoot(document.getElementById("root")).render(
+  <App />
+);
